8968ce581105a03e02e02542ee4c3ac2bc42f52c
tanreaper <72271517+tanreaper@users.noreply.github.com>
2020-11-10 17:15:02 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-api_key = "AIzaSyDNv-LHjwfFui7ecgaQsNGOdXxITCGOK-k"
-
-mysql_host = 'pathlearn.thehomeserver.net'
-mysql_user = 'PathLearnFlask'
-mysql_password = 'W6ee,~c.97@bHQbB'
-mysql_db = 'test'
